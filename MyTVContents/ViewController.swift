12f4d016820a1ebddca390d0e56ab4410d37a58c
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-01-30 16:45:55 +0900

    Initial Commit

---
+++ MyTVContents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTVContents
+//
+//  Created by 조유진 on 1/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
